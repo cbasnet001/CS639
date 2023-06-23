759e127d19552cf2109a3f581020754f3aed3d77
chetanpixel <cbasnet130@gmail.com>
2023-06-22 23:56:29 -0400

    Draft1
    General structure without implementation

---
+++ Project/Gamify/app/src/main/java/com/example/gamify/AddExpenseActivity.java
@@ -0,0 +1,98 @@
+package com.example.gamify;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.gamify.databinding.ActivityAddExpenseBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.ktx.Firebase;
+
+import java.util.Calendar;
+import java.util.UUID;
+
+public class AddExpenseActivity extends AppCompatActivity {
+    ActivityAddExpenseBinding binding;
+    private String type;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityAddExpenseBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        type=getIntent().getStringExtra("type");
+        if (type.equals("Income")){
+            binding.incomeRadio.setChecked(true);
+        }else{
+            binding.expenseRadio.setChecked(true);
+        }
+        binding.incomeRadio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                type="Income";
+            }
+        });
+        binding.expenseRadio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                type="Expense";
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.add_menu,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id==R.id.saveExpense){
+            createExpense();
+            return true;
+        }
+        return false;
+    }
+
+    private void createExpense() {
+        String expenseId= UUID.randomUUID().toString();
+        String amount = binding.amount.getText().toString();
+        String note = binding.note.getText().toString();
+        String category = binding.category.getText().toString();
+
+        boolean incomeChecked = binding.incomeRadio.isChecked();
+
+        if (incomeChecked){
+            type="Income";
+        } else{
+            type="Expense";
+
+        }
+
+
+        if (amount.trim().length()==0){
+            binding.amount.setError("Empty");
+            return;
+        }
+        ExpenseModel expenseModel=new ExpenseModel(expenseId,note,category,type,Long.parseLong(amount), Calendar.getInstance().getTimeInMillis());
+
+        FirebaseFirestore
+                .getInstance()
+                .collection("expenses")
+                .document(expenseId)
+                .set(expenseModel);
+        finish();
+
+    }
+}
